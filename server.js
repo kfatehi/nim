70029814c0b2d419933d9140827ca692feeddf90
keyvan <keyvanfatehi@gmail.com>
2011-07-14 07:29:34 -0700

    web nimbus requests

--- server.js
+++ server.js
@@ -1,26 +1,36 @@
 #!/usr/bin/env node
 // SERVER
-process.on('uncaughtException', function (err) {
-  // Just a failsafe--or in case we're getting fuzzed :P
-  console.error(err.stack);
-});
-
-// need to install redis....
 var crypto = require('crypto');
 var shasum = crypto.createHash('sha1');
 var net = require('net');
+var http = require('http');
+var url = require('url');
 var redis = require("redis").createClient();
-
+process.on('uncaughtException', function (err) {
+  console.error(err.stack);
+});
 redis.on("error", function (err) {
     console.log("Redis client error: " + err);
 });
 
-var generateId = function () {
-  var shasum = crypto.createHash('sha1');
-  shasum.update(new Date().getTime().toString());
-  return shasum.digest('hex').slice(0,6);
-}
 
+var doWebBufferRequest = function (id, socket) {
+  console.log('Web request for: '+id);
+  redis.exists('nimbus:'+id, function (err, reply) {
+    if (reply)
+      redis.get('nimbus:'+id, function (err, buffer) {
+        if (err) {
+          socket.end('error: Redis error while retrieving buffer.');
+          console.error('  Redis-related error. Web display failed: '+id);
+          console.error(err);
+        } else {
+          socket.end(buffer);
+          console.log('Web server sent buffer for nimbus: '+id);
+        }
+      });
+    else socket.end('Nothing found with id: '+id);
+  });
+}
 var doCreateNewNimbus = function (socket) {
   var id = generateId();
   redis.exists('nimbus:'+id, function (err, reply) {
@@ -32,9 +42,8 @@ var doCreateNewNimbus = function (socket) {
     }
   });
 }
-
 var doSeedBuffer = function (data, id, socket) {
-  var buffer = data.slice(20, data.length);
+  var buffer = data.slice(18, data.length);
   console.log('Seeding buffer: '+id);
   redis.set('nimbus:'+id, buffer, function (err, reply) {
     if (reply == 'OK') {
@@ -48,7 +57,11 @@ var doSeedBuffer = function (data, id, socket) {
     }
   });
 }
-
+var generateId = function () {
+  var shasum = crypto.createHash('sha1');
+  shasum.update(new Date().getTime().toString());
+  return shasum.digest('hex').slice(0,6);
+}
 var server = net.createServer(function (socket) {
   socket.setEncoding("UTF8");
   socket.addListener("data", function (data) {
@@ -62,23 +75,33 @@ var server = net.createServer(function (socket) {
         var id = params[0].slice(0, 5);
       switch (message) {
         case 'join_nimbus':{
-          var nimbus_id = params[0];
-          console.log('Client is joining nimbus: '+nimbus_id);
-          redis.get('nimbus:'+nimbus_id, function (err, buffer) {
+          var id = params[0];
+          console.log('Client is joining nimbus: '+id);
+          redis.get('nimbus:'+id, function (err, buffer) {
             if (err) {
               socket.write('error: Redis error while retrieving buffer.');
               console.error('  Redis-related error. Buffer seeding failed: '+id);
               console.error(err);
             } else {
-              socket.write('seed_buffer:'+nimbus_id+'>'+buffer);
-              console.log('Sent buffer for nimbus: '+nimbus_id);
+              socket.write('seed_buffer:'+id+'>'+buffer);
+              console.log('Sent buffer for nimbus: '+id);
             }
           });
           break;
         }
         case 'create_new_nimbus':{ doCreateNewNimbus(socket); break; }
         case 'seed_buffer':{ doSeedBuffer(data, id, socket); break; }
-        default: console.log('Unknown message: '+data);
+        default: {
+          if (/HTTP/i.test(message)) {
+            var id = message.match(/GET \/(.*) HTTP/)[1];
+            if (id) doWebBufferRequest(id, socket);
+            else socket.end('Welcome to the Nimbus web server.');
+          }
+          else {
+            console.log('Unknown message: '+data);
+            socket.end('What? '+data);
+          }
+        }
       }
     }
   });
